4b9b9e21085c15bcbf49e2856a5dadf65604b4ca
= <60747023+mlilloblanco@users.noreply.github.com>
2020-08-06 13:15:07 -0400

    F

---
+++ src/test/java/cl/awake/psegurito/test/junit/Asesoria.java
@@ -0,0 +1,57 @@
+package cl.awake.psegurito.test.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/servlet-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+@WebAppConfiguration
+class UsuariosRestControllerTest {
+
+	private MockMvc mvc;
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+
+	
+	@Test
+	void AsesoriaTest() throws Exception {
+		mvc.
+	}
+	
+//	@Test
+//	void verListadoTest() throws Exception {
+//		// Simular petición a la API
+//		MvcResult resultado = mvc
+//				.perform(
+//						get("/api/usuarios").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isOk()).andReturn();
+//
+//		// Convertir el resultado a String
+//		String content = resultado.getResponse().getContentAsString();
+//
+//		// Validar resultado
+//		assertTrue(content.contains("admin@detta.cl"));
+//	}
+
+}
