1462b48f99a38aef60f4d4dbe23ab791a7d577bd
= <60747023+mlilloblanco@users.noreply.github.com>
2020-08-07 15:49:22 -0400

    JUnit

--- src/test/java/cl/awake/psegurito/test/junit/ProfesionalServiceTest.java
+++ src/test/java/cl/awake/psegurito/test/junit/ProfesionalServiceTest.java
@@ -2,18 +2,39 @@ package cl.awake.psegurito.test.junit;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cl.awake.psegurito.model.Profesional;
+import cl.awake.psegurito.services.ProfesionalService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:root-context.xml")
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING) //Forzar a hacer las pruebas unitarias en orden según el nombre de los test
 public class ProfesionalServiceTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+	@Autowired
+	private ProfesionalService profesionalService;
+	
+//	@Before
+//	public void setUp() throws Exception {
+//	}
+	
+	/**
+	 * Cantidad de profesionales en la lista, en el inicio de esta lista tenemos 7 profesionales creados
+	 */
+	@Test
+	public void TestListar() {
+		List<Profesional> listaProfesionales = profesionalService.getAll();
+		int cantidad = 7;
+		assertEquals(cantidad, listaProfesionales.size());
+	}
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
-    }
 
 }
